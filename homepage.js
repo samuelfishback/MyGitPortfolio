880d8f41ce885a67202d45c58ebcd12f36b3455e
Sam Fishback <smf15shback@gmail.com>
2024-08-22 22:59:53 -0700

    transition added for carousel up and scale down

--- homepage.js
+++ homepage.js
@@ -2,9 +2,8 @@ const aboutMeUpArrow = document.getElementById('aboutMeUpArrow');
 const myAboutMeItems = document.getElementsByClassName('my-about-me-items');
 
 function carouselUpArrow() {
-  console.log('clicked');
-  // translate in css x:-50% to center card, stays consistent
-  myAboutMeItems[0].style.translate = "-50% -100%";
+  // translate property in css x:-50% to center card, however transform is a new property and can stack with translate
+  myAboutMeItems[0].style.transform = "scale(0.8) translate(0, -120%)";
 }
 
 aboutMeUpArrow.addEventListener('click', carouselUpArrow);
