299dab8b5b39189a315ce9396af256de06a614a8
Sam Fishback <smf15shback@gmail.com>
2024-08-26 23:52:35 -0700

    deleted transition, moved to animation with keyframes

--- homepage.js
+++ homepage.js
@@ -1,9 +1,10 @@
+const aboutMeSquare = document.getElementById('aboutMeSquare');
 const aboutMeUpArrow = document.getElementById('aboutMeUpArrow');
 const myAboutMeItems = document.getElementsByClassName('my-about-me-items');
 
 function carouselUpArrow() {
   // translate property in css x:-50% to center card, however transform is a new property and can stack with translate
-  myAboutMeItems[0].style.transform = "scale(0.8) translate(0, -120%)";
+  myAboutMeItems[0].className = 'animation-playback';
 }
 
 aboutMeUpArrow.addEventListener('click', carouselUpArrow);
