38449b333d4f971a4b2d9ddd17378c2f3f6e8d24
Sam Fishback <smf15shback@gmail.com>
2024-08-28 23:49:16 -0700

    changed element.className property to element.classList.add function

--- homepage.js
+++ homepage.js
@@ -4,7 +4,14 @@ const myAboutMeItems = document.getElementsByClassName('my-about-me-items');
 
 function carouselUpArrow() {
   // translate property in css x:-50% to center card, however transform is a new property and can stack with translate
-  myAboutMeItems[0].className = 'animation-playback';
+  myAboutMeItems[0].classList.add('animation-playback');
+}
+
+// TESTING
+function checkClassArray() {
+  console.log(myAboutMeItems.length);
+  console.log(myAboutMeItems[0].classList);
 }
 
 aboutMeUpArrow.addEventListener('click', carouselUpArrow);
+aboutMeSquare.addEventListener('click', checkClassArray);
