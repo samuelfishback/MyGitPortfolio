d90f8ea47b54d6511261390679b40c1f3558544c
samuelfishback <79609086+samuelfishback@users.noreply.github.com>
2025-01-30 22:01:26 -0800

    since we are dealing with javascript function animations, removing the classes is necessary. classAdd is removed from JS. added new TODO

--- homepage.js
+++ homepage.js
@@ -6,9 +6,9 @@ var buttonPresses = 0;
 
 /* 
 TODO: 
-  * Create variable that increments whenever carouselUpArrow is called.
-  * Place class add into switch statements to go through each array element (there is a better way but i dont know it yet)
-  * When variable reaches 3 and cards are in orginal positions, reset variable and remove anim classes.
+  * add function for keeping time and animation duration
+  * create function for ease out formula
+  * create function for moving boxes (animation)
 */
 
 // Starts at zero on page load. After three, resets to one, not zero
@@ -22,31 +22,6 @@ function carouselIncrement() {
 
 function carouselUpArrow() {
   switch (buttonPresses) {
-  // translate property in css x:-50% to center card, however transform is a new property and can stack with translate
-    case 1:
-      aboutMeArray[0].classList.add('animation-playback');
-  /* this is fun and great but not functional for this task
-  aboutMeArray.push(aboutMeArray[0]);
-  aboutMeArray.shift();
-  */
-      aboutMeArray[1].classList.add('animation-playforward');
-      aboutMeArray[2].classList.add('animation-playmiddle');
-  // removing animations resets positions but not Array index because anim are set in classes
-  // setTimeout(removeAnimations, 2000);
-      break;
-    case 2:
-      aboutMeArray[0].classList.add('animation-playmiddle');
-      aboutMeArray[1].classList.add('animation-playback');
-      aboutMeArray[2].classList.add('animation-playforward');
-      break;
-  }
-}
-
-/* A function that should work if eventListener is placed on a good element 
-   Maybe use a setTimeout(function, seconds) event instead*/
-function removeAnimations() {
-  for(let i=0;i<aboutMeArray.length;i++) {
-    aboutMeArray[i].classList.remove('animation-playforward', 'animation-playmiddle', 'animation-playback');
   }
 }
 
